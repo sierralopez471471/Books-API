d3c82a149fad5b45658cd6d035eb5c18bccc3a37
Sierra Lopez <sierralopez1006@gmail.com>
2022-11-22 17:41:23 -0800

    installed and configure cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //DEPENDENCIES
 const mongoose = require('mongoose')
 const express = require('express')
+const cors = require('cors')
 
 //CONFIGURATIONS
 require('dotenv').config()
@@ -15,6 +16,7 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
 //MIDDLEWARE
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cors())
 const booksController = require('./controllers/book-controller.js')
 app.use('/books', booksController)
 
